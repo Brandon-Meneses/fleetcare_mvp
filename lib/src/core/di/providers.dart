ebac83209cf2091c5fee97cc5a7cae14f5d1c7f5
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2025-09-12 02:11:41 -0500

    chore(di): integrar repositorio de mantenimiento en el inyector de dependencias

--- lib/src/core/di/providers.dart
+++ lib/src/core/di/providers.dart
@@ -3,11 +3,15 @@ import '../../features/fleet/data/bus_repository_impl.dart';
 import '../../features/fleet/domain/repositories/bus_repository.dart';
 import '../../features/maintenance/data/maintenance_repository_impl.dart';
 import '../../features/maintenance/domain/repositories/maintenance_repository.dart';
+import '../../features/maintenance/data/api_maintenance_repository.dart';
 
 final busRepositoryProvider = Provider<BusRepository>((ref) {
   // Más adelante cambias a Isar/Supabase sin tocar la UI
   return InMemoryBusRepository();
 });
 
-final maintenanceRepositoryProvider =
-Provider<MaintenanceRepository>((_) => InMemoryMaintenanceRepository());
+final maintenanceRepositoryProvider = Provider<MaintenanceRepository>((ref) {
+  // 🔹 Cambia aquí según quieras probar
+  // return InMemoryMaintenanceRepository();
+  return ApiMaintenanceRepository();
+});
